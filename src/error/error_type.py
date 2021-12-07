c2fb8ef5e266ba6b6c5a126cad5f9059f204f168
milosob <git@milosob.com>
2021-12-07 21:12:52 +0100

    add comments

--- src/error/error_type.py
+++ src/error/error_type.py
@@ -1,15 +1,21 @@
+# GENERIC
 INTERNAL_SERVER_ERROR = "internal-server-error"
 SERVICE_UNAVAILABLE = "service-unavailable"
 
+# AUTHORIZATION
 AUTHORIZATION_ACCESS_TOKEN_INVALID = "authorization-access-token-invalid"
 AUTHORIZATION_ACCESS_TOKEN_EXPIRED = "authorization-access-token-expired"
 AUTHORIZATION_REFRESH_TOKEN_INVALID = "authorization-refresh-token-invalid"
 AUTHORIZATION_REFRESH_TOKEN_EXPIRED = "authorization-refresh-token-expired"
 
+# ACCOUNT REGISTER
 ACCOUNT_REGISTER_USERNAME_TAKEN = "account-register-username-taken"
+
+# ACCOUNT AUTHENTICATE
 ACCOUNT_AUTHENTICATE_INVALID_CREDENTIALS = "account-authenticate-invalid-credentials"
 ACCOUNT_AUTHENTICATE_UNAUTHORIZED_SCOPES = "account-authenticate-unauthorized-scopes"
 
+# CONFIRM
 CONFIRM_TOKEN_INVALID = "confirm-token-failure"
 CONFIRM_TOKEN_NOT_FOUND = "confirm-token-not-found"
 CONFIRM_TOKEN_EXPIRED = "confirm-token-expired"
